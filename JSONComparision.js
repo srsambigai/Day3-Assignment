aaa5fbc02c549ad5e7e04ecd75d65a46dd8cfafc
Swarna Senthil <srsambigai@gmail.com>
2023-10-25 18:42:57 -0500

    Day3 assignment added with three files

---
+++ JSONComparision.js
@@ -0,0 +1,17 @@
+let obj1={name :"Person1",age :4};
+let obj2={age :5,name:"Person2"};
+if((obj1.name)===(obj2.name))
+{
+    console.log("Obj1 and Obj2 have same name:",obj1.name);
+}
+else{
+    console.log("Obj1 and Obj2 have different name:",obj1.name," and ",obj2.name);
+}
+
+if((obj1.age)===(obj2.age))
+{
+    console.log("Obj1 and Obj2 are same age:",obj1.age);
+}
+else{
+    console.log("Obj1 and Obj2 are different age:",obj1.age," and ",obj2.age);
+}
